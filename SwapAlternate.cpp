23488422727ed1e45cff4cc14339ac25bfd58064
khushivj711 <khushi.vijay8711@gmail.com>
2024-09-21 10:02:23 +0000

    swap the alternate values in array

---
+++ SwapAlternate.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void SwapAlternate(int arr[] ,int size){
+
+    for(int i = 0 ;i<size ; i=i+2){
+        if((i+1) < size){
+            swap(arr[i] , arr[i+1]);
+        }
+    }
+}
+
+void printArray(int arr[] , int size){
+
+    for(int i = 0 ;i<size ; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+
+    int arr[6] = {1,3,2,7,11,8};
+
+    SwapAlternate(arr , 6);
+    printArray(arr , 6);
+
+    return 0;
+}
