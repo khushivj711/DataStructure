a1fe73c6dacc80c9d658019595eaac8e42f3e058
khushivj711 <khushi.vijay8711@gmail.com>
2024-09-21 03:15:54 +0000

    find the sum of array

---
+++ arrayUpdate.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void update(int arr[] , int size){
+    cout<<"Inside the function"<<endl;
+
+    arr[0] = 120;
+    for(int i = 0 ; i<size ; i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+
+    cout<<"Going back to function"<<endl;
+}
+
+int SumArray(int arr , int size){
+    int sum = 0;
+    for(int i = 0 ; i<size ; i++){
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+int main(){
+    int arr[3] = {1,2,3} ;
+
+    update(arr , 3);
+
+    for(int i = 0 ; i<3 ; i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
